2c7b7426f90bdad436653c88c33eba0399761788
Joseph Spurrier <github@josephspurrier.com>
2022-02-15 21:11:40 -0500

    Move in cloud storage

--- pkg/cloudstorage/cloudstorage.go
+++ pkg/cloudstorage/cloudstorage.go
@@ -3,6 +3,8 @@
 package cloudstorage
 
 import (
+	"os"
+
 	"github.com/ambientkit/ambient"
 	"github.com/ambientkit/ambient/pkg/envdetect"
 	"github.com/ambientkit/plugin/storage/awsbucketstorage"
@@ -30,4 +32,30 @@ func StorageBasedOnCloud(sitePath string, sessionPath string) ambient.StoragePlu
 	}
 
 	return storage
+}
+
+// RunningInAWS returns true if running in AWS services. When running in
+// App Runner, it will be set: AWS_EXECUTION_ENV=AWS_ECS_FARGATE.
+func RunningInAWS() bool {
+	_, exists := os.LookupEnv("AWS_EXECUTION_ENV")
+	return exists
+}
+
+// RunningInAWSLambda returns true if running in AWS Lambda.
+func RunningInAWSLambda() bool {
+	_, exists := os.LookupEnv("AWS_LAMBDA_FUNCTION_NAME")
+	return exists
+}
+
+// RunningInGoogle returns true if running in Google. When running in
+// Google Cloud Run, will be set: K_SERVICE=NAME.
+func RunningInGoogle() bool {
+	_, exists := os.LookupEnv("K_SERVICE")
+	return exists
+}
+
+// RunningInAzureFunction returns true if running in Azure Functions.
+func RunningInAzureFunction() bool {
+	_, exists := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT")
+	return exists
 }
