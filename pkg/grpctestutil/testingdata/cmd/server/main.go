2c90aeb5cec906d73905658355bd2baa49e1da32
Joseph Spurrier <github@josephspurrier.com>
2022-03-28 18:43:42 -0400

    Quick update for testing

--- pkg/grpctestutil/testingdata/cmd/server/main.go
+++ pkg/grpctestutil/testingdata/cmd/server/main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"log"
 
+	"github.com/ambientkit/ambient"
 	"github.com/ambientkit/plugin/pkg/grpctestutil"
 )
 
 func main() {
-	app, err := grpctestutil.Setup2(true)
+	app, err := grpctestutil.StandardSetup(true)
+	//app, err := grpctestutil.GRPCSetup(true)
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
+
+	app.PluginSystem().SetGrant("hello", ambient.GrantPluginNeighborGrantRead)
 
 	h, err := app.Handler()
 	if err != nil {
